5fc5439c80281672fa14475cca083292998f3485
Gnidleif <tf@outpost24.com>
2016-11-14 10:09:09 +0100

    Started developing bad_spellcheck and synced time on CentOS

---
+++ bad_spellcheck.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python3.5
+import sys
+from random import randint
+
+def randChar(c):
+    s = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
+    i = s.index(c)
+    r = randint(-1, 1)
+    n = ''
+    if d != 0:
+        i += r
+        n = s[i] if 
+    c = n
+    return 
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("usage: {} <args..>".format(sys.argv[0]))
+        
+    for cmd in sys.argv[1:]:
+        print(cmd)
