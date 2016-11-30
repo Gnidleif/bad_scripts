d8924d339744386360561bf1661e8db8e29f7e1d
gnidleif <gnidleif@gmail.com>
2016-11-30 09:22:42 +0100

    Aliveclock added

---
+++ aliveclock.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import sys
+import time
+from datetime import datetime
+
+def calcTime():
+    count = 0
+    goal = 60
+    lastTick = int(time.time())
+    while count < goal:
+        now = int(time.time())
+        if now > lastTick:
+            count += 1
+            lastTick = now
+            print("{}/{}".format(count, goal))
+    return "Our estimations say that you're alive now."
+
+if __name__ == "__main__":
+        if len(sys.argv) < 2:
+            print("usage: {} <YYMMDD>".format(sys.argv[0]))
+            sys.exit(1337)
+
+        try:
+            birthday = datetime.strptime(sys.argv[1], '%y%m%d').strftime('%Y-%m-%d')
+        except ValueError as ve:
+            print("Required date format: YYMMDD")
+            sys.exit(1337)
+        except Exception as e:
+            print("Exception: {}".format(e))
+            sys.exit(1337)
+
+        print("Since your birthday is {}, we need some time to calculate your alive clock.".format(birthday))
+        print(calcTime())
