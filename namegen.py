2cb77ca5a1f611fa6058a00cc2c2abc118540042
gnidleif <gnidleif@gmail.com>
2017-01-02 11:17:58 +0100

    Removed space

--- namegen.py
+++ namegen.py
@@ -14,7 +14,6 @@ def nameGen(name):
     name = list(name)
     for i in range(1, len(name)):
         name[i] = vowels[randint(0, len(vowels) - 1)] if name[i] in vowels else chr(randint(0, 25) + 97)
-
     return str(''.join(name))
 
 if __name__ == "__main__":
