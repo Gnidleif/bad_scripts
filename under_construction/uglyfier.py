a2e25ea3eee83891ef1df2d53246b335b4104928
gnidleif <gnidleif@gmail.com>
2017-01-17 11:30:18 +0100

    Minor text changes

--- under_construction/uglyfier.py
+++ under_construction/uglyfier.py
@@ -3,6 +3,23 @@ import sys
 import json
 import re
 
+def init(type):
+    with open('lookup.json', 'r') as f:
+        regexes = json.load(f)
+        
+    if type not in regexes.keys():
+        print("Supported formats: {}".format([x for x in regexes.keys()]))
+        return None
+    
+    regexes = regexes[type]
+    for i in range(len(regexes)):
+        try:
+            regexes[i] = re.compile(regexes[i], flags=re.I)
+        except TypeError as te:
+            print("Arg: [{}] raised TypeError: {}".format(regexes[i], te))
+            return None
+    return regexes
+
 def produce(data, regexes):
     to_switch = {}
     processed = data
@@ -17,24 +34,14 @@ if __name__ == "__main__":
         print("usage: {} <source> <destination>".format(sys.argv[0]))
         exit(1337)
     
-    src, dest = sys.argv[1], sys.argv[2]
+    src, dst = sys.argv[1], sys.argv[2]
     type = src.split('.')[-1]
-    with open('lookup.json', 'r') as f:
-        regexes = json.load(f)
-        
-    if type not in regexes.keys():
-        print("Supported formats: {}".format([x for x in regexes.keys()]))
+    regexes = init(type)
+    if regexes is None:
         exit(1337)
-    
-    regexes = regexes[type]
-    for i in range(len(regexes)):
-        try:
-            regexes[i] = re.compile(regexes[i], flags=re.I)
-        except TypeError as te:
-            print("Arg: [{}] raised TypeError: {}".format(regexes[i], te))
-            exit(1337)
         
     with open(src, 'r') as f:
         data = f.read()
     
-    result = produce(data, regexes)
+    with open(dst, 'w') as f:
+        f.write("{}\n".format(produce(data, regexes)))
